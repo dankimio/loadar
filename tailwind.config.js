945ecfa78991b09dbc0b6d17adb24a19797bb149
Dan Kim <itsdanya@gmail.com>
2024-10-27 01:12:57 +0300

    Configure theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
+    themes: [
+      {
+        loadar: {
+          "color-scheme": "light",
+          primary: "rgb(22, 163, 74)",
+          "primary-content": "#fff",
+          secondary: "oklch(69.71% 0.329 342.55)",
+          "secondary-content": "oklch(98.71% 0.0106 342.55)",
+          accent: "oklch(76.76% 0.184 183.61)",
+          neutral: "#2B3440",
+          "neutral-content": "#D7DDE4",
+          "base-100": "oklch(100% 0 0)",
+          "base-200": "#F2F2F2",
+          "base-300": "#E5E6E6",
+          "base-content": "#1f2937",
+        },
+      },
+    ],
   },
 };
