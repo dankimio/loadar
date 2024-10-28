cc226e8481853a911e4f503bdf592fa2085cacd3
Dan Kim <itsdanya@gmail.com>
2024-10-28 23:47:47 +0300

    Add bids#create test

--- test/controllers/bids_controller_test.rb
+++ test/controllers/bids_controller_test.rb
@@ -1,9 +1,22 @@
 require "test_helper"
 
 class BidsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in_company
+    @bid = bids(:one)
+  end
+
   test "should get new" do
     sign_in_company
     get new_bid_url
     assert_response :success
+  end
+
+  test "should create bid" do
+    assert_difference("Bid.count") do
+      post bids_url, params: {
+        bid: @bid.attributes.slice("company_name", "route", "load").merge(price: @bid.price - 1) }
+    end
+    assert_redirected_to new_bid_url(bid: { route: @bid.route, load: @bid.load })
   end
 end
