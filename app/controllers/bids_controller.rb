c4714c76a46aab77ec3ad062a77161033b34ca01
Dan Kim <itsdanya@gmail.com>
2024-10-28 22:50:43 +0300

    Use bids#new for step 1

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,7 +1,7 @@
 class BidsController < ApplicationController
   def new
     if current_company_name
-      @bid = Bid.new(company_name: current_company_name, route: params[:route], load: params[:load])
+      @bid = Bid.new(bid_params)
       @job = @bid.job
     else
       redirect_to new_session_path
@@ -13,7 +13,8 @@ class BidsController < ApplicationController
     @job = @bid.job
 
     if @bid.save
-      redirect_to new_bid_path(route: @bid.route, load: @bid.load), notice: "Your bid has been submitted"
+      redirect_to new_bid_path(bid: { route: @bid.route, load: @bid.load }),
+        notice: "Your bid has been submitted"
     else
       render :new, status: :unprocessable_entity
     end
@@ -22,12 +23,12 @@ class BidsController < ApplicationController
   private
 
   def bid_params
-    params.require(:bid).permit(:company_name, :route, :load, :price)
+    params.fetch(:bid, {}).permit(:route, :load, :price).merge(company_name: current_company_name)
   end
 
   def find_bid
     bid = Bid.find_by(company_name: current_company_name, route: bid_params[:route], load: bid_params[:load])
-    bid.price = bid_params[:price] if bid
+    bid.price = bid_params[:price] if bid && bid_params[:price]
     bid || Bid.new(bid_params)
   end
 end
