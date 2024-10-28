3c200824a82f75b3cda9b7f96e9b7651a3ebfe78
Dan Kim <itsdanya@gmail.com>
2024-10-28 19:57:36 +0300

    Add and validate routes

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,6 +1,13 @@
 class Bid < ApplicationRecord
+  ROUTES = {
+    "berlin-warsaw" => [ "Berlin", "Warsaw" ],
+    "belfast-cardiff" => [ "Belfast", "Cardiff" ],
+    "prague-brussels" => [ "Prague", "Brussels" ],
+    "amsterdam-cologne" => [ "Amsterdam", "Cologne" ]
+  }
+
   validates :company_name, presence: true, uniqueness: { scope: [ :route, :load ] }
-  validates :route, presence: true
+  validates :route, presence: true, inclusion: { in: ROUTES.keys }
   validates :load, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
 end
